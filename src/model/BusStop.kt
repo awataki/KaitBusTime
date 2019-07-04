0d73852f6a09f163528c2c8dafefb5ddc63d0132
awataki <awataki1206@gmail.com>
2019-07-04 23:40:58 +0900

    add enum values

--- src/model/BusStop.kt
+++ src/model/BusStop.kt
@@ -1,10 +1,13 @@
 package model
 
-enum class BusStop {
-    CENTER,
-    STATION,
-    ODORI,
-    SHORENJI,
-    OGINOSINSYUKU,
-    CAMPUS,
+enum class BusStop(i:Int) {
+    CENTER(0),
+    STATION(1),
+    ODORI(2),
+    SHORENJI(3),
+    OGINOSINSYUKU(4),
+    CAMPUS(5);
+    companion object {
+        fun fromID(i:Int):BusStop{return BusStop.values()[i]}
+    }
 }
--- src/model/BusType.kt
+++ src/model/BusType.kt
@@ -1,7 +1,11 @@
 package model
 
-enum class BusType {
-    ROUTE,
-    DIRECT,
-    SHUTTLE,
+enum class BusType(val i: Int) {
+    DIRECT(0),
+    ROUTE(1),
+    SHUTTLE(2);
+
+    companion object{
+        fun fromID(i:Int):BusType{return BusType.values()[i]}
+    }
 }
