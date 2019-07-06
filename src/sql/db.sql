cd742562374ad6e70ef548503de83b4c1616b77b
awataki <awataki1206@gmail.com>
2019-07-06 18:07:11 +0900

    create sqlfile

---
+++ src/sql/db.sql
@@ -0,0 +1,14 @@
+create table time_table
+(
+    id      int auto_increment,
+    hour    int not null,
+    minutes int not null,
+    weekday int not null,
+    type    int not null,
+    ftom    int not null,
+    `to`    int not null,
+    next    int null,
+    constraint time_table_pk
+        primary key (id)
+);
+
