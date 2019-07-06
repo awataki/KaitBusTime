a23120779aadcbe3f65bb03f6c892acfb03f6ca6
awataki <awataki1206@gmail.com>
2019-07-06 18:01:36 +0900

    define model.WeekType

--- src/model/WeekType.kt
+++ src/model/WeekType.kt
@@ -1,6 +1,13 @@
 package model
 
-enum class WeekType {
-    fun toInt(): Int {
+enum class WeekType(val value: kotlin.Int) {
+    WEEKDAY(0),
+    HOLIDAY(1),
+    BOTH(2);
+
+    companion object {
+        fun fromValue(i: Int): WeekType {
+            return WeekType.values()[i]
+        }
     }
 }
