0eedc863f83b92278ebc6fedd4473fd3f5914b06
awataki <awataki1206@gmail.com>
2019-07-08 00:14:03 +0900

    fix table

--- src/sql/db.sql
+++ src/sql/db.sql
@@ -1,13 +1,13 @@
 create table time_table
 (
-    id      int auto_increment,
-    hour    int not null,
-    minutes int not null,
-    weekday int not null,
-    type    int not null,
-    from    int not null,
-    `to`    int not null,
-    next    int null,
+    id        int auto_increment,
+    hour      int not null,
+    minutes   int not null,
+    weekday   int not null,
+    type      int not null,
+    from      int not null,
+    `to`      int not null,
+    direction int null,
     constraint time_table_pk
         primary key (id)
 );
