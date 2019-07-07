1a8f1eff606048ec5f65d367510fd65b9496109e
awataki <awataki1206@gmail.com>
2019-07-08 08:23:41 +0900

    define integration test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -4,6 +4,11 @@ import com.google.gson.Gson
 import io.ktor.http.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import model.BusStop
+import model.BusType
+import model.Time
+import model.WeekType
+import java.time.LocalTime
 
 class ApplicationTest {
     @Test
@@ -12,11 +17,27 @@ class ApplicationTest {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.MethodNotAllowed, response.status())
             }
-            handleRequest(HttpMethod.Get, "/get/999").apply {
+//            handleRequest(HttpMethod.Get, "/get/999").apply {
+//                val gson = Gson()
+//                assertEquals(HttpStatusCode.OK, response.status())
+//                // TODO Insert Test Data
+//                assertEquals(gson.toJson("Test Data Here"), response.content)
+//            }
+            handleRequest(HttpMethod.Get, "/get/next?direction=0").apply {
                 val gson = Gson()
                 assertEquals(HttpStatusCode.OK, response.status())
-                // TODO Insert Test Data
-                assertEquals(gson.toJson("Test Data Here"), response.content)
+                assertEquals(
+                    gson.toJson(
+                        Time(
+                            1,
+                            LocalTime.of(23, 59),
+                            WeekType.BOTH,
+                            BusType.DIRECT,
+                            BusStop.CENTER,
+                            BusStop.ODORI
+                        )
+                    ), response.content
+                )
             }
         }
     }
