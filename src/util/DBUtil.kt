2c6265d32a35949e8aa131abbaf7bb165059e394
awataki <awataki1206@gmail.com>
2019-07-08 20:32:30 +0900

    fix privatevalue to lower

--- src/util/DBUtil.kt
+++ src/util/DBUtil.kt
@@ -5,10 +5,10 @@ import exception.DBConnectException
 import java.sql.Connection
 
 class DBUtil {
-    private val Address = Config.DB_URI
-    private val DBUser = Config.DB_USER
-    private val DBPassword = Config.DB_PASSWORD
-    private val db = java.sql.DriverManager.getConnection(Address, DBUser, DBPassword)
+    private val dbURI = Config.DB_URI
+    private val dbUser = Config.DB_USER
+    private val dbPassword = Config.DB_PASSWORD
+    private val db = java.sql.DriverManager.getConnection(dbURI, dbUser, dbPassword)
 
     fun connect(): Connection {
         return this.db ?: throw DBConnectException()
